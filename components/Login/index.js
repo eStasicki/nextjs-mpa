41b08bc6f87fa7e9df7149ff4533d521e79094d8
Ernest Stasicki <estasicki@gmail.com>
2020-09-11 00:59:12 +0200

    Add Login page auth

--- components/Login/index.js
+++ components/Login/index.js
@@ -1,9 +1,52 @@
 import styles from './Login.module.scss'
-import classNames from "classnames/bind";
-import { NextSeo } from 'next-seo';
-let cx = classNames.bind(styles);
+import classNames from "classnames/bind"
+import { NextSeo } from 'next-seo'
+let cx = classNames.bind(styles)
+import { useState, setState } from 'react'
+import { setCookie } from 'nookies'
+import Router from 'next/router'
 
 function Login() {
+
+  const siteUrl = 'http://backend.estasicki.pl/myprojects';
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  async function handleLogin() {
+    const loginInfo = {
+      username: username,
+      password: password,
+    }
+
+    const login = await fetch(`${siteUrl}/wp-json/jwt-auth/v1/token`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginInfo)
+    })
+
+    const loginResponse = await login.json();
+
+    if( loginResponse.token != undefined ) {
+
+      setCookie(null, 'jwt', loginResponse.token, {
+        maxAge: 30 * 24 * 60 * 60,
+        path: '/',
+      })
+
+      Router.push('/');
+      console.log(loginResponse);
+    }
+    else {
+      alert(loginResponse.message)
+    }
+    //Router.push('/');
+    //console.log(loginResponse);
+
+  }
+
   return (
     <>
     <NextSeo
@@ -15,14 +58,14 @@ function Login() {
         <form>
           <div className={cx("email-box")}>
             <p>Adres e-mail</p>
-            <input type="text" />
+            <input type="text" onChange={e => setUsername(e.target.value)} value={username} />
           </div>
           <div className={cx("password-box")}>
             <p>Hasło:</p>
-            <input type="password"/>
+            <input type="password" onChange={e => setPassword(e.target.value)} value={password}/>
           </div>
           <div className={cx("buttons-box")}>
-            <button type="submit">Zaloguj się</button>
+            <button type="button" onClick={ () => handleLogin() } >Zaloguj się</button>
             <button className={cx("register")}>Rejestracja</button>
           </div>
         </form>
