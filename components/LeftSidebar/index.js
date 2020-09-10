1cabb0cb227e538b26140b902d07b071ecb1db56
Ernest Stasicki <estasicki@gmail.com>
2020-09-10 19:17:50 +0200

    Sidebar - add menu items with GraphQL

--- components/LeftSidebar/index.js
+++ components/LeftSidebar/index.js
@@ -1,5 +1,6 @@
 import Link from '@/components/Link'
-
+import { Query } from "react-apollo"
+import { getSidebarMenu } from "@/queries/getSidebarMenu"
 import styles from './LeftSidebar.module.scss'
 import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
@@ -7,18 +8,31 @@ let cx = classNames.bind(styles);
 function LeftSidebar() {
   return (
     <div className={cx("wrapper")}>
-      <ul>
-      <li>
-        <Link activeClassName={cx("active")} href="/">
-          <a>Przegląd projektów</a>
-        </Link>
-      </li>
-      <li>
-        <Link activeClassName={cx("active")} href="/projects">
-          <a className='nav-link'>Projekty</a>
-        </Link>
-      </li>
-    </ul>
+      <Query query={getSidebarMenu}>
+        {({ loading, error, data }) => {
+          if (loading)
+          return (
+            <div>Wczytywanie menu...</div>
+          )
+          if (error) return false
+          return (
+            <ul>
+                { data.menuItems.nodes.length > 0 ? 
+                  <>
+                  {data.menuItems.nodes.map((menu) => (
+                    <li>
+                      <Link activeClassName={cx("active")} href={menu.url}>
+                        <a>{menu.label}</a>
+                      </Link>
+                    </li>
+                  ))}
+                  </>
+                  : "Brak pozycji w menu."
+                }
+            </ul>
+          )
+        }}
+      </Query>
     </div>
   )
 }
---
+++ queries/getSidebarMenu.js
@@ -0,0 +1,15 @@
+import gql from "graphql-tag"
+
+export const getSidebarMenu = gql`
+  {
+    menuItems(where: {location: SIDEBAR_MENU}) {
+      nodes {
+        id
+        label
+        url
+      }
+    }
+  }
+`
+
+export default getSidebarMenu
