36cdc0c065122c54b202d1bcad497e4f6bfbda72
Ernest Stasicki <estasicki@gmail.com>
2020-09-14 23:25:36 +0200

    Add CommunicationTopMenu as Component

--- components/CommunicationTopMenu/index.js
+++ components/CommunicationTopMenu/index.js
@@ -1,23 +1,35 @@
-import styles from './Header.module.scss';
+import { Query } from 'react-apollo';
+import Link from '@/components/Link';
+import { getCommunicationTopMenu } from '@/queries/getCommunicationTopMenu';
+import styles from '@/components/CommunicationTopMenu/CommunicationTopMenu.module.scss';
 import classNames from 'classnames/bind';
 let cx = classNames.bind(styles);
 
-function CommunicationTopMenu({ username, userGroup }) {
+function CommunicationTopMenu() {
   return (
-    <ul className={cx('top-bar')}>
-      <li>
-        <a href="#">Nowa wiadomość</a>
-      </li>
-      <li>
-        <a href="">Skrzynka odbiorcza</a>
-      </li>
-      <li>
-        <a href="">Skrzynka nadawcza</a>
-      </li>
-      <li>
-        <a href="">Rozliczenia</a>
-      </li>
-    </ul>
+    <Query query={getCommunicationTopMenu}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Wczytywanie...</p>;
+        if (error) return false;
+        return (
+          <ul className={cx('top-bar')}>
+            {data.menuItems.nodes.length > 0 ? (
+              <>
+                {data.menuItems.nodes.map((menu) => (
+                  <li key={menu.id}>
+                    <Link activeClassName={cx('active')} href={menu.url}>
+                      <a>{menu.label}</a>
+                    </Link>
+                  </li>
+                ))}
+              </>
+            ) : (
+              'Brak pozycji w menu.'
+            )}
+          </ul>
+        );
+      }}
+    </Query>
   );
 }
 
--- pages/communication/index.js
+++ pages/communication/index.js
@@ -1,38 +1,11 @@
 import { NextSeo } from 'next-seo';
-import Link from '@/components/Link';
-import { Query } from 'react-apollo';
-import { getCommunicationTopMenu } from '@/queries/getCommunicationTopMenu';
-import styles from '@/components/CommunicationTopMenu/CommunicationTopMenu.module.scss';
-import classNames from 'classnames/bind';
-let cx = classNames.bind(styles);
+import CommunicationTopMenu from '@/components/CommunicationTopMenu';
 
 function Communication() {
   return (
     <>
       <NextSeo title={'Komunikacja'} description={'Opis strony tutaj'} />
-      <Query query={getCommunicationTopMenu}>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Wczytywanie...</p>;
-          if (error) return false;
-          return (
-            <ul className={cx('top-bar')}>
-              {data.menuItems.nodes.length > 0 ? (
-                <>
-                  {data.menuItems.nodes.map((menu) => (
-                    <li key={menu.id}>
-                      <Link activeClassName={cx('active')} href={menu.url}>
-                        <a>{menu.label}</a>
-                      </Link>
-                    </li>
-                  ))}
-                </>
-              ) : (
-                'Brak pozycji w menu.'
-              )}
-            </ul>
-          );
-        }}
-      </Query>
+      <CommunicationTopMenu />
     </>
   );
 }
